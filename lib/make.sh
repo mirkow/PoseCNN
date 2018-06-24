e787c66497d7d29ceb9a242e12d65d68276089ce
Mirko Waechter <mirko.waechter@kit.edu>
2018-06-24 17:58:42 +0200

    added missing expt-relaxed-constexpr flag

--- lib/make.sh
+++ lib/make.sh
@@ -9,7 +9,7 @@ export CPLUS_INCLUDE_PATH=/usr/include/Eigen3
 cd hard_label_layer
 
 nvcc -std=c++11 -c -o hard_label_op.cu.o hard_label_op_gpu.cu.cc \
-	-I $TF_INC -I$TF_INC/external/nsync/public -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_50
+	-I $TF_INC -I$TF_INC/external/nsync/public -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_50 --expt-relaxed-constexpr
 
 g++ -std=c++11 -shared -o hard_label.so hard_label_op.cc \
 	hard_label_op.cu.o -I $TF_INC -I$TF_INC/external/nsync/public -fPIC -lcudart -lcublas -L $CUDA_PATH/lib64 -L$TF_LIB -ltensorflow_framework
